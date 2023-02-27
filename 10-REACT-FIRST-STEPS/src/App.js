09220d58aa2a461018cd2ad2c908ea06329c973a
Ruben-Alvarez-Dev <info@ruben-alvarez.com>
2023-02-27 12:08:13 +0100

    Add REACT-FIRST-STEPS

---
+++ 10-REACT-FIRST-STEPS/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import MiComponente from './MiComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Word, from React!
+        </p>
+      {/* Cargo MiComponente */}
+      <MiComponente/>
+      <MiComponente/>
+      
+      </header>
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ 10-REACT-FIRST-STEPS/src/MiComponente.js
@@ -0,0 +1,19 @@
+// Importar modulos de react / dependencias
+import React from "react";
+
+// Función del componente
+const MiComponente = () => {
+
+    return (<div className="MiComponente">
+                <h2>MiComponente</h2>
+                <hr></hr>
+
+                <ul>
+                    <li>Hola 1</li>
+                    <li>Hola 2</li>
+                </ul>
+            </div>);
+}
+
+// Export
+export default MiComponente;
