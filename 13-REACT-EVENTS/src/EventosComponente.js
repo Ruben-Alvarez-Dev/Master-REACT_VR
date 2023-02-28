b7611dfb5ea86661902b48644e0acef46a364bb0
Ruben-Alvarez-Dev <info@ruben-alvarez.com>
2023-02-28 13:11:46 +0100

    Add 13-REACT-EVENTS-ONFOCUS_ONBLUR

--- 13-REACT-EVENTS/src/EventosComponente.js
+++ 13-REACT-EVENTS/src/EventosComponente.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const EventosComponente = () => {
   
-    const hasDadoClick = (e, nombre) => {
-        alert("Click" + nombre);
-    }
+    // const hasDadoClick = (e, nombre) => {
+    //     alert("Click" + nombre);
+    // }
 
-    function hasDadoDobleClick(e){
-        alert("Doble Click");
-    }
+    // function hasDadoDobleClick(e){
+    //     alert("Doble Click");
+    // }
 
     // const hasEntrado = (e) => {
     //     alert("Has ENTRADO con el MOUSE")
@@ -18,11 +18,18 @@ export const EventosComponente = () => {
     //     alert("Has SALIDO con el MOUSE")
     // }
 
-    const hasEntrado = (e, accion) => {
-        alert(`Has ${accion} a la caja`);
-        console.log(`Has ${accion} a la caja`);
+    // const hasEntrado = (e, accion) => {
+    //     alert(`Has ${accion} a la caja`);
+    //     console.log(`Has ${accion} a la caja`);
+    // }
+
+    const estasDentro = e => {
+        console.log("Estás DENTRO del INPUT")
     }
 
+    const estasFuera = e => {
+        console.log("Estás FUERA del INPUT")
+    }
    return (
     <div>
         <h2>Eventos en React</h2>
@@ -35,15 +42,15 @@ export const EventosComponente = () => {
         BOTON
         </button> */}
 
-        {/* Evento click, VERSION LLAMADA A FUNCION */}
+        {/* Evento click, VERSION LLAMADA A FUNCION
         <button onClick={ e => hasDadoClick(e, "Ruben")}>CLICK</button>
         
         {/* Evento doble click */}
-        <button onDoubleClick={hasDadoDobleClick}>DOBLE CLICK</button>
+        {/* <button onDoubleClick={hasDadoDobleClick}>DOBLE CLICK</button> */}
 
         {/* Evento onMouseEnter y onMouseLeave */}
 
-        <div    id="caja" 
+        {/* <div    id="caja" 
                 // onMouseEnter={hasEntrado}
                 // onMouseLeave={hasSalido}>
 
@@ -52,7 +59,15 @@ export const EventosComponente = () => {
 
             
             Pasa por encima!
-        </div>
+        </div> */}
+
+        <p>
+            <input type="text"
+                onFocus={ estasDentro }
+                onBlur={ estasFuera }
+                placeholder="Introduce tu nombre"/>
+
+        </p>
 
 
     </div>
